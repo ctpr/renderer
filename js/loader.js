b44dcf53452faf6f906f8990205d69e664ba69af
Chris Russ <chruss@gmx.de>
2017-04-09 02:31:43 +1000

    Further clean-up and removal of unused dependencies

--- js/loader.js
+++ js/loader.js
@@ -5,21 +5,8 @@ angular.module('demo', [])
         url: 'http://35.158.16.68:9000/geometries'
     }).then(function successCallback(response) {
         // asynchronous
-        // $scope.response = response;
         updateGeometries(response.data.paths);
     }, function errorCallback(response) {
         // asynchronous
     });
 });
-// angular.module('demo', [])
-// .controller('ModelLoader', function($scope, $http) {
-//     $http({
-//       method: 'GET',
-//       url: 'http://35.158.16.68:9000/geometries/sample_data_scalar.ply'
-//     }).then(function successCallback(response) {
-//         // asynchronous
-//         loadModels(response.data);
-//       }, function errorCallback(response) {
-//         // asynchronous
-//       });
-// });
