7c8346c4c8cdf33897486ceb8c95097eb55efe77
Chris Russ <chruss@gmx.de>
2017-07-12 05:04:30 +1000

    Testing DNS entry over hard coded IP address

--- js/loader.js
+++ js/loader.js
@@ -2,7 +2,7 @@ angular.module('demo', [])
 .controller('ModelSelector', function($scope, $http) {
     $http({
         method: 'GET',
-        url: 'http://35.158.16.68:9000/geometries'
+        url: 'http://geometry.duckdns.org:9000/geometries'
     }).then(function successCallback(response) {
         // asynchronous
         updateGeometries(response.data.paths);
